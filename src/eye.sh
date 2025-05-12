8dd6a811f1cb8a2fecbebebc29e2b3638f736fcd
rickNoise <emberix@gmail.com>
2025-05-12 15:21:25 +1000

    finished trhough CH3 L5, but code is quite messy and not accounting for all possible user error

--- src/eye.sh
+++ src/eye.sh
@@ -1,3 +1,3 @@
 # this is an example of how to run targeted unit tests
-python3 -m unittest test_md_process_fns.TestSplitNodesImage -v
+# python3 -m unittest test_md_process_fns.TestSplitNodesImage -v
 python3 -m unittest test_md_process_fns.TestSplitNodesLink -v
--- src/test_md_process_fns.py
+++ src/test_md_process_fns.py
@@ -289,6 +289,25 @@ class TestSplitNodesImage(unittest.TestCase):
             new_nodes,
             expected_output
         )
+    
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
 
 class TestSplitNodesLink(unittest.TestCase):
     def test_split_nodes_link(self):
