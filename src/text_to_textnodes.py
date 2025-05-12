e5c230c544958d4b03855eb780842fe2575b267c
rickNoise <emberix@gmail.com>
2025-05-12 15:42:07 +1000

    cleaned up small thing in text_to_textnodes

--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -3,20 +3,18 @@ from md_process_fns import *
 
 def text_to_textnodes(text):
     print(f"RUNNING TEXT_TO_TEXTNODES with input {text}")
-    starting_input = [TextNode(text, TextType.TEXT)]
-    final_node_list = []
+    nodes = [TextNode(text, TextType.TEXT)]
 
     # BOLD text
-    final_node_list = split_nodes_delimiter(starting_input, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
     # ITALIC text
-    final_node_list = split_nodes_delimiter(final_node_list, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     # CODE text
-    final_node_list = split_nodes_delimiter(final_node_list, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
 
     # IMAGE text
-    final_node_list = split_nodes_image(final_node_list)
+    nodes = split_nodes_image(nodes)
     # LINK text
-    final_node_list = split_nodes_link(final_node_list)
+    nodes = split_nodes_link(nodes)
 
-    print(f"about to return {final_node_list}")
-    return final_node_list
+    return nodes
